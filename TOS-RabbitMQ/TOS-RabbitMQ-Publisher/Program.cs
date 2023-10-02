28d413246ab67ad332250565b2199780d3ea891f
ThibautMonin <thibaut.monin@diiage.org>
2023-10-02 21:29:23 +0200

    add .net console projects

---
+++ TOS-RabbitMQ/TOS-RabbitMQ-Publisher/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost", UserName = "user", Password = "password"};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "tos", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+const string message = "TOS RabbitMQ";
+
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: string.Empty, routingKey: "tos", basicProperties: null, body: body);
+
+Console.WriteLine($"Sent {message}");
+Console.ReadLine();
---
+++ TOS-RabbitMQ/TOS-RabbitMQ-Subscriber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost", UserName = "user", Password = "password"};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "tos", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+Console.WriteLine("Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Received {message}");
+};
+channel.BasicConsume(queue: "tos",
+    autoAck: true,
+    consumer: consumer);
+
+Console.WriteLine(" Press a key to exit.");
+Console.ReadLine();
